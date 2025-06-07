b11e4eb9da886b22b949238db605b9db4f42bb41
notdvrk781 <hfsguhfssb@gmail.com>
2025-06-07 23:12:22 +0300

    Universal Lock.lua

--- Universal Lock.lua
+++ Universal Lock.lua
@@ -1,4 +1,4 @@
-﻿local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Universal Lock",
